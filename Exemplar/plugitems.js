2068df158f6172647ed2db7d6ff59cd8183a877b
raihle <jraihle@gmail.com>
2012-02-02 09:45:10 +0100

    Exemplar now uses absolute positioning for most elements to make generation easier.

--- Exemplar/plugitems.js
+++ Exemplar/plugitems.js
@@ -23,20 +23,14 @@ function populateList() {
 		var price = get(item, "price");
 		var picture = get(item, "picture");
 		var description = get(item, "description");
-		var text = document.getElementById("result-list").innerHTML;
-				
-		text = text.concat("<div class=\"item\" id=\"" + i + "\">");
+		var text = document.getElementById("resultlist_1").innerHTML;
 		
-		text = text.concat("<div class=\"title\"><h3>" + name
-				+ "</h3></div>");
-		text = text.concat("<div class=\"price\"><p>Price: <span class=\"value\">" + price
-				+ "</span></p></div>");
-
-		text = text.concat("<div class=\"picture\"><img width=\"100px\" height=\"100px\" src=\"" + picture + "\"/></div>");
-		
-		text = text.concat("<div class=\"description\"><p>" + description
-				+ "</p></div>");
+		text = text.concat("<div class=\"item\" id=\"item_" + i + "\" style=\"top:" + i * 135 + "px;\">");
+		text = text.concat("	<h3 class=\"title\">" + name + "</h3>");
+		text = text.concat("	<p class=\"price\">Price: <span class=\"value\">" + price + "</span></p>");
+		text = text.concat("	<img class=\"picture\" width=\"100px\" height=\"100px\" src=\"" + picture + "\"/>");
+		text = text.concat("	<p class=\"description\">" + description + "</p>");
 		text = text.concat("</div>");
-		document.getElementById("result-list").innerHTML = text;
+		document.getElementById("resultlist_1").innerHTML = text;
 	}
 }
